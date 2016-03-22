16daed7a6ed6edbe421ada1b82f0df6abb126920
Christian Nicolaide <cnicolaide@gmail.com>
2016-03-22 17:00:47 -0300

    Commit Inicial

---
+++ src/minacarbon/MinaCarbon.java
@@ -0,0 +1,13 @@
+package minacarbon;
+
+import ProcesadorPetri.Matriz;
+
+public class MinaCarbon {
+
+    public static void main(String[] args) {
+        
+        Matriz miMatriz = new Matriz();
+
+    }
+
+}
