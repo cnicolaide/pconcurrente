ab46c4c9af97a7c2fcbae03e771a6d61484cac36
Christian <cnicolaide@gmail.com>
2016-11-01 16:49:18 -0300

    Reacomodo estructura de archivos para que el wrapper de gradle funcione bien

--- src/test/java/unitTest/TestMonitor.java
+++ src/test/java/unitTest/TestMonitor.java
@@ -1,4 +1,4 @@
-package Test;
+package unitTest;
 
 import java.util.HashMap;
 import java.util.logging.Logger;
--- src/test/java/unitTest/TestProcesador.java
+++ src/test/java/unitTest/TestProcesador.java
@@ -1,4 +1,4 @@
-package Test;
+package unitTest;
 
 import static org.junit.Assert.*;
 
